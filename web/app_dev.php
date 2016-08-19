b59c0349837d33c4496267c1a2f33a9573eef30d
CELLIER Florian <cellier.florian@hotmail.fr>
2016-08-19 14:39:32 +0200

    Project cacheclear file add ignore docker

--- web/app_dev.php
+++ web/app_dev.php
@@ -4,17 +4,21 @@ use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Debug\Debug;
 
 // If you don't want to setup permissions the proper way, just uncomment the following PHP line
-// read http://symfony.com/doc/current/book/installation.html#checking-symfony-application-configuration-and-setup
-// for more information
+// read http://symfony.com/doc/current/book/installation.html#configuration-and-setup for more information
 //umask(0000);
+$ip2longCli = ip2long(@$_SERVER['REMOTE_ADDR']);
+$minIp = ip2long('172.17.0.1');
+$maxIp = ip2long('172.17.0.10');
 
 // This check prevents access to debug front controllers that are deployed by accident to production servers.
 // Feel free to remove this, extend it, or make something more sophisticated.
+// I custom by remove this instruction in the condition '|| isset($_SERVER['HTTP_X_FORWARDED_FOR'])' because it's the header send by xdebug
 if (isset($_SERVER['HTTP_CLIENT_IP'])
-    || isset($_SERVER['HTTP_X_FORWARDED_FOR'])
-    || !(in_array(@$_SERVER['REMOTE_ADDR'], ['127.0.0.1', 'fe80::1', '::1']) || php_sapi_name() === 'cli-server')
+    || (!(in_array(@$_SERVER['REMOTE_ADDR'], array('127.0.0.1', 'fe80::1', '::1'))) AND !(($minIp <= $ip2longCli) && ($ip2longCli <= $maxIp)) )
+    	|| php_sapi_name() === 'cli-server'
 ) {
     header('HTTP/1.0 403 Forbidden');
+	echo @$_SERVER['REMOTE_ADDR'],
     exit('You are not allowed to access this file. Check '.basename(__FILE__).' for more information.');
 }
 
