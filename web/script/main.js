19173f04a713d6e8c38c00b15a98a6a657b04120
Max <maxime.bressange@gmail.com>
2016-09-23 13:13:45 +0000

    Add de la vue profil

--- web/script/main.js
+++ web/script/main.js
@@ -1,13 +1,33 @@
 /*SIDE SIDEBAR*/
-$("#menu-toggle").click(function(e) {
-    e.preventDefault();
-    if ($( "#wrapper" ).hasClass( "toggled" )){
-        $("#wrapper").removeClass("toggled");
-        $("#menu-toggle").addClass("pushed");
-    }
-    else{
-        $("#wrapper").addClass("toggled");
-        $("#menu-toggle").removeClass("pushed");
-    }
-});
+function clickToggle() {
+  $("#menu-toggle").click(function(e) {
+      e.preventDefault();
+      if ($( "#wrapper" ).hasClass( "toggled" )){
+          $("#wrapper").removeClass("toggled");
+          $("#menu-toggle").addClass("pushed");
+      }
+      else{
+          $("#wrapper").addClass("toggled");
+          $("#menu-toggle").removeClass("pushed");
+      }
+  });
+};
 /* FIN SIDENAV*/
+/*   Thumbnail     */
+
+function thumbnail() {
+  $(".thumbnail").on("click", function(){
+		$(".thumbnail").animate({
+			width: "15em",
+			height: "10em",
+			duration: 0.25
+		});
+		$().css("visibility:", "");
+	});
+}
+/*   Fin Thumbnail     */
+
+$(document).ready(function() {
+  clickToggle();
+  thumbnail();
+})
