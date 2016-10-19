6539a876e34a054440d18e5163f80a6b035c4a71
CELLIER Florian <cellier.florian@hotmail.fr>
2016-10-19 13:55:54 +0200

    Profile mise en forme

--- src/AppBundle/Form/FOSUser/ProfilUserType.php
+++ src/AppBundle/Form/FOSUser/ProfilUserType.php
@@ -6,7 +6,9 @@ use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 use AppBundle\Entity\User;
+use FOS\UserBundle\Util\LegacyFormHelper;
 
 
 class ProfilUserType extends AbstractType
@@ -16,19 +18,33 @@ class ProfilUserType extends AbstractType
   public function buildForm(FormBuilderInterface $builder, array $options)
   {
       $builder
-          ->add('firstname', null, array(
-            'label' => 'Nom'
-          ))
-          ->add('lastname', null, array(
-            'label' => 'Prénom'
-          ))
+          ->remove('current_password')
+          ->remove('username')
+          ->remove('email')
           ->add('gender', ChoiceType::class, array(
+            'expanded' => true,
+            'multiple' => false,
             'choices'  => array(
                 'Monsieur' => User::GENDER_MALE,
                 'Madame' => User::GENDER_FEMALE,
             ),
-            'label' => 'Civilité'
+            'label' => 'Civilité :*',
+            'required' => true
           ))
+          ->add('username', null, array('label' => 'Nom d\'utilisateur*',
+            'translation_domain' => 'FOSUserBundle',
+            'required' => true
+            ))
+          ->add('firstname', TextType::class, array(
+            'label' => 'Nom*',
+            'required' => true
+          ))
+          ->add('lastname', null, array(
+            'label' => 'Prénom*',
+            'required' => true
+          ))
+          ->add('email', LegacyFormHelper::getType('Symfony\Component\Form\Extension\Core\Type\EmailType'),
+              array('label' => 'form.email', 'translation_domain' => 'FOSUserBundle'))
         ;
   }
     /**
